22450cac66a4dc27f31883a88e1829b1e899ad75
makerlabworkshop <88671450+makerlabworkshop@users.noreply.github.com>
2021-11-20 12:30:10 +0100

    Update Dronestreaming.py

--- SurveillanceDrone/Dronestreaming.py
+++ SurveillanceDrone/Dronestreaming.py
@@ -1,1 +1,21 @@
 
+import cv2
+from djitellopy import *
+
+# create an instance of tello drone
+my = Tello()
+my.connect()
+my.streamon()
+
+while True:
+    img = my.get_frame_read().frame
+    img = cv2.resize(img, (400,360))
+    cv2.imshow("Drone Streaming ",img)
+    if cv2.waitKey(25) and 0xFF==ord('q'):
+        break
+
+
+
+
+
+
