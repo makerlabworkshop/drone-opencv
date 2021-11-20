bedb1b676e59918ccdb0cc4239f9137de0c20145
makerlabworkshop <88671450+makerlabworkshop@users.noreply.github.com>
2021-11-20 13:08:34 +0100

    Update Dronestreaming.py

--- SurveillanceDrone/Dronestreaming.py
+++ SurveillanceDrone/Dronestreaming.py
@@ -1,18 +1,36 @@
-
 import cv2
 from djitellopy import *
+from tkinter import *
+import tkinter
+counter = 0
 
+# Initialize the drone
 # create an instance of tello drone
 my = Tello()
 my.connect()
 my.streamon()
 
+# build interface
+w = Tk()
+w.geometry("400x300")
+# definition of take image function
+def takeImage():
+    global counter = counter+1
+    img = my.get_frame_read().frame
+    cv2.imwrite(f'images/image-{counter}.png',img)
+
+# -----button----------
+btn = Button(text="Take Photo", command=takeImage)
+btn.pack()
+# Streaming
 while True:
     img = my.get_frame_read().frame
     img = cv2.resize(img, (400,360))
     cv2.imshow("Drone Streaming ",img)
     if cv2.waitKey(25) and 0xFF==ord('q'):
         break
+    w.mainloop()
+
 
 
 
